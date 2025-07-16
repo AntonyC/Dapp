2035dacb8a8da519dc7f270532093396c86baacd
Antony <antony.cn@icloud.com>
2025-07-16 21:19:58 +0800

    Update task1-solidity.sol

--- base2-smart-contracts/task1-solidity.sol
+++ base2-smart-contracts/task1-solidity.sol
@@ -1,9 +1,28 @@
-### 1.  ✅ 创建一个名为Voting的合约，包含以下功能：
-- 一个mapping来存储候选人的得票数
-- 一个vote函数，允许用户投票给某个候选人
-- 一个getVotes函数，返回某个候选人的得票数
-- 一个resetVotes函数，重置所有候选人的得票数
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
 
+// ### 1.  ✅ 创建一个名为Voting的合约，包含以下功能：
+// - 一个mapping来存储候选人的得票数
+// - 一个vote函数，允许用户投票给某个候选人
+// - 一个getVotes函数，返回某个候选人的得票数
+// - 一个resetVotes函数，重置所有候选人的得票数
+
+contract Voting {
+    mapping(address => mapping (uint256 => uint256)) private  _voteCounts;
+    uint256 private _version = 0;
+
+    function vote(address candidate) external {
+        _voteCounts[candidate][_version] += 1;
+    }
+
+    function getVotes(address candidate) external view returns (uint256) {
+        return _voteCounts[candidate][_version];
+    }
+
+    function resetVotes() external{
+        _version++;
+    }
+}
 
 ### 2. ✅ 反转字符串 (Reverse String)
 - 题目描述：反转一个字符串。输入 "abcde"，输出 "edcba"
