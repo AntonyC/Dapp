81e33126242d903c061b9aaf698bd207f7817d26
Antony <antony.cn@icloud.com>
2025-07-27 12:30:09 +0800

    add dotenv

--- smart-contracts/harthat-demo/hardhat.config.ts
+++ smart-contracts/harthat-demo/hardhat.config.ts
@@ -1,8 +1,21 @@
-import type { HardhatUserConfig } from "hardhat/config";
+import {type HardhatUserConfig, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import 'dotenv/config';
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks:{
+    sepolia: {
+      url:`https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts:[`${process.env.PK}`]
+    }
+  },
 };
 
 export default config;
+
+task("greett", "Greets a user")
+  .addParam("name", "The name of the user")
+  .setAction(async (taskArgs) => {
+    console.log(`Hello, ${taskArgs.name}!`);
+  });
