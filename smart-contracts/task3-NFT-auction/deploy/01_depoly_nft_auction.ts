0c5a5e55e3671d981dbc37eb18c635a64de1cc27
Antony <antony.cn@icloud.com>
2025-08-01 23:57:38 +0800

    change js to ts

---
+++ smart-contracts/task3-NFT-auction/deploy/01_depoly_nft_auction.ts
@@ -0,0 +1,30 @@
+// module.exports = async ({getNamedAccounts, deployments}) => {
+//   const {deployIfDifferent, log, save} = deployments;
+//   const {deployer} = await getNamedAccounts();
+//   console.log('deployer---:', deployer);
+
+//   // const deployResult = await deploy('Token', {
+//   //   from: deployer,
+//   //   args: ['hello', 100],
+//   // });
+//   // if (deployResult.newlyDeployed) {
+//   //   log(
+//   //     `contract Token deployed at ${deployResult.address} using ${deployResult.receipt.gasUsed} gas`
+//   //   );
+//   // }
+// };
+// module.exports.tags = ['Token'];
+
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
+
+const func: DeployFunction = async function ({
+	getNamedAccounts,
+	deployments,
+}: HardhatRuntimeEnvironment) {
+	const { deployer } = await getNamedAccounts();
+	console.log('deployer---:', deployer);
+};
+
+export default func;
+func.tags = ['AntonyAuction'];
