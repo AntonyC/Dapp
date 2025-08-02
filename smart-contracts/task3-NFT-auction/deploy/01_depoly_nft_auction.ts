85519f8cce497b37970b665454860f996cba741d
Antony <antony.cn@icloud.com>
2025-08-02 23:48:44 +0800

    refactor

--- smart-contracts/task3-NFT-auction/deploy/01_depoly_nft_auction.ts
+++ smart-contracts/task3-NFT-auction/deploy/01_depoly_nft_auction.ts
@@ -2,16 +2,39 @@ import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
 import path from 'path';
 import fs from 'fs';
+import { ethers, upgrades } from 'hardhat';
 
-const func: DeployFunction = async function ({
-	getNamedAccounts,
-	deployments,
-	ethers,
-	upgrades,
-}: HardhatRuntimeEnvironment) {
-	const { deployer } = await getNamedAccounts();
+interface DeployData {
+	proxyAddress: string;
+	implAddress: string;
+	abi: string[];
+}
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+	const { deployer } = await hre.getNamedAccounts();
 	console.log('deployer---:', deployer);
 
+	const deployData = await getDeployData();
+	saveToLocalCache(deployData, 'proxyAuction.json');
+
+	await hre.deployments.save('auctionProxy', {
+		abi: deployData.abi,
+		address: deployData.proxyAddress,
+		// args: [],
+		// log: true,
+	});
+	// await deploy('AntonyAuction', { from: deployer, log: true });
+};
+
+export default func;
+func.tags = ['AntonyAuction'];
+
+function saveToLocalCache(deployData: DeployData, fileName: string) {
+	const storePath = path.resolve(__dirname, `./.cache/${fileName}`);
+	fs.mkdirSync(path.dirname(storePath), { recursive: true });
+	fs.writeFileSync(storePath, JSON.stringify(deployData));
+}
+
+async function getDeployData(): Promise<DeployData> {
 	const antonyAuction = await ethers.getContractFactory('AntonyAuction');
 	const auctionProxy = await upgrades.deployProxy(antonyAuction, [], {
 		initializer: 'initialize',
@@ -23,25 +46,9 @@ const func: DeployFunction = async function ({
 	const implAddress = await upgrades.erc1967.getImplementationAddress(proxyAddress);
 	console.log('impl Address: ', implAddress);
 
-	// const storePath = path.resolve(__dirname, './.cache/proxyAuction.json');
-	// fs.mkdirSync(path.dirname(storePath), { recursive: true });
-	// fs.writeFileSync(
-	// 	storePath,
-	// 	JSON.stringify({
-	// 		proxyAddress,
-	// 		implAddress,
-	// 		abi: antonyAuction.interface.format(false),
-	// 	})
-	// );
-
-	await deployments.save('auctionProxy', {
-		abi: antonyAuction.interface.format(true),
-		address: proxyAddress,
-		// args: [],
-		// log: true,
-	});
-	// await deploy('AntonyAuction', { from: deployer, log: true });
-};
-
-export default func;
-func.tags = ['AntonyAuction'];
+	return {
+		proxyAddress,
+		implAddress,
+		abi: antonyAuction.interface.format(false),
+	};
+}
