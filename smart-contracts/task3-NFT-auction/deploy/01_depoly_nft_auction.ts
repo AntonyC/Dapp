e30ff7376092f2d5ecdcf1ab08c30a9fd30989cf
Antony <antony.cn@icloud.com>
2025-08-02 13:07:47 +0800

    add deploy

--- smart-contracts/task3-NFT-auction/deploy/01_depoly_nft_auction.ts
+++ smart-contracts/task3-NFT-auction/deploy/01_depoly_nft_auction.ts
@@ -1,29 +1,16 @@
-// module.exports = async ({getNamedAccounts, deployments}) => {
-//   const {deployIfDifferent, log, save} = deployments;
-//   const {deployer} = await getNamedAccounts();
-//   console.log('deployer---:', deployer);
-
-//   // const deployResult = await deploy('Token', {
-//   //   from: deployer,
-//   //   args: ['hello', 100],
-//   // });
-//   // if (deployResult.newlyDeployed) {
-//   //   log(
-//   //     `contract Token deployed at ${deployResult.address} using ${deployResult.receipt.gasUsed} gas`
-//   //   );
-//   // }
-// };
-// module.exports.tags = ['Token'];
-
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
+import { viem, upgrades } from 'hardhat';
 
 const func: DeployFunction = async function ({
 	getNamedAccounts,
 	deployments,
 }: HardhatRuntimeEnvironment) {
+	const { deploy } = deployments;
 	const { deployer } = await getNamedAccounts();
 	console.log('deployer---:', deployer);
+
+	await deploy('AntonyAuction', { from: deployer, log: true });
 };
 
 export default func;
--- smart-contracts/task3-NFT-auction/hardhat.config.ts
+++ smart-contracts/task3-NFT-auction/hardhat.config.ts
@@ -1,14 +1,15 @@
-import type { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox-viem";
-import "hardhat-deploy";
+import type { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox-viem';
+import 'hardhat-deploy';
+import '@openzeppelin/hardhat-upgrades';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
-  namedAccounts: {
-    deployer: 0,
-    user1: 1,
-    user2: 2
-  },
+	solidity: '0.8.28',
+	namedAccounts: {
+		deployer: 0,
+		user1: 1,
+		user2: 2,
+	},
 };
 
 export default config;
