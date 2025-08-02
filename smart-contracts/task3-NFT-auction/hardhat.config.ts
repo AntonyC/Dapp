c125265d12740b7684ed67b42834c8e543452f98
Antony <antony.cn@icloud.com>
2025-08-02 22:40:59 +0800

    add docenv and task

--- smart-contracts/task3-NFT-auction/hardhat.config.ts
+++ smart-contracts/task3-NFT-auction/hardhat.config.ts
@@ -1,10 +1,17 @@
-import type { HardhatUserConfig } from 'hardhat/config';
+import { type HardhatUserConfig, task } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox-viem';
+import 'dotenv/config';
 import 'hardhat-deploy';
 import '@openzeppelin/hardhat-upgrades';
 
 const config: HardhatUserConfig = {
 	solidity: '0.8.28',
+	networks: {
+		sepolia: {
+			url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+			accounts: [`${process.env.PK}`],
+		},
+	},
 	namedAccounts: {
 		deployer: 0,
 		user1: 1,
@@ -13,3 +20,8 @@ const config: HardhatUserConfig = {
 };
 
 export default config;
+
+task('env', 'Print env values').setAction(async hre => {
+	console.log('-INFURA_API_KEY-: ', process.env.INFURA_API_KEY);
+	console.log('-PK-: ', process.env.PK);
+});
