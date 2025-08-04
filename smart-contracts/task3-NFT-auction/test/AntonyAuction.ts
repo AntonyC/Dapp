d21e6928f83fc90f24b9c0cbbbadcdc878f0c496
Antony <antony.cn@icloud.com>
2025-08-05 00:19:11 +0800

    Add test

--- smart-contracts/task3-NFT-auction/test/AntonyAuction.ts
+++ smart-contracts/task3-NFT-auction/test/AntonyAuction.ts
@@ -2,29 +2,13 @@ import { time, loadFixture } from '@nomicfoundation/hardhat-toolbox-viem/network
 import { expect, use } from 'chai';
 import hre from 'hardhat';
 import { getAddress, parseGwei } from 'viem';
+import { TestERC20 } from '../typechain-types';
 
 describe('AntonyAuction', function () {
-	async function deployAntonyAutionFixture() {
-		const [owner, user1, user2] = await hre.viem.getWalletClients();
-		// const antonyAuction = await hre.viem.deployContract('AntonyAuction');
-		await hre.deployments.fixture(['DeployAntonyAuction']);
-		const nftAuctionProxy = await hre.deployments.get('auctionProxy');
-		const antonyAuction = await hre.ethers.getContractAt('AntonyAuction', nftAuctionProxy.address);
-		const publicClient = await hre.viem.getPublicClient();
-
-		return {
-			antonyAuction,
-			owner,
-			user1,
-			user2,
-			publicClient,
-		};
-	}
-
 	describe('Deployment', function () {
 		it('Test the admin is owner', async function () {
-			const { antonyAuction, owner } = await loadFixture(deployAntonyAutionFixture);
-			expect(await antonyAuction.admin()).to.equal(getAddress(owner.account.address));
+			const { antonyAuction, deployer } = await loadFixture(deployAntonyAutionFixture);
+			expect(await antonyAuction.admin()).to.equal(getAddress(deployer.account.address));
 		});
 
 		it('Create the auction', async function () {
@@ -38,4 +22,60 @@ describe('AntonyAuction', function () {
 			expect(await antonyAuction.nextAuctionId()).to.equal(1n);
 		});
 	});
+
+	async function deployAntonyAutionFixture() {
+		const [deployer, user1, user2] = await hre.viem.getWalletClients();
+
+		const [signer, buyer] = await hre.ethers.getSigners();
+		console.log('--aaaaaaaaaaaaa: ', signer.address, hre.ethers.version, hre.ethers.getSigners);
+		// const antonyAuction = await hre.viem.deployContract('AntonyAuction');
+		await hre.deployments.fixture(['DeployAntonyAuction']);
+		const nftAuctionProxy = await hre.deployments.get('auctionProxy');
+		const antonyAuction = await hre.ethers.getContractAt('AntonyAuction', nftAuctionProxy.address);
+		const publicClient = await hre.viem.getPublicClient();
+
+		// const testERC20 = await hre.viem.deployContract('TestERC20');
+		const TestERC20 = await hre.ethers.getContractFactory('TestERC20');
+		const testERC20 = (await TestERC20.deploy()) as TestERC20;
+		await testERC20.waitForDeployment();
+		const UsdcAddress = await testERC20.getAddress();
+
+		let tx = await testERC20.connect(signer).transfer(buyer, hre.ethers.parseEther('1000'));
+		await tx.wait();
+
+		const aggreagatorV3 = await hre.ethers.getContractFactory('AggreagatorV3');
+		const priceFeedEthDeploy = await aggreagatorV3.deploy(hre.ethers.parseEther('10000'));
+		const priceFeedEth = await priceFeedEthDeploy.waitForDeployment();
+		const priceFeedEthAddress = await priceFeedEth.getAddress();
+		console.log('ethFeed: ', priceFeedEthAddress);
+		const priceFeedUSDCDeploy = await aggreagatorV3.deploy(hre.ethers.parseEther('1'));
+		const priceFeedUSDC = await priceFeedUSDCDeploy.waitForDeployment();
+		const priceFeedUSDCAddress = await priceFeedUSDC.getAddress();
+		console.log('usdcFeed: ', await priceFeedUSDCAddress);
+
+		const token2Usd = [
+			{
+				token: hre.ethers.ZeroAddress,
+				priceFeed: priceFeedEthAddress,
+			},
+			{
+				token: UsdcAddress,
+				priceFeed: priceFeedUSDCAddress,
+			},
+		];
+
+		for (let i = 0; i < token2Usd.length; i++) {
+			const { token, priceFeed } = token2Usd[i];
+			await antonyAuction.setPriceFeed(token, priceFeed);
+		}
+
+		return {
+			antonyAuction,
+			deployer,
+			user1,
+			user2,
+			publicClient,
+			testERC20,
+		};
+	}
 });
