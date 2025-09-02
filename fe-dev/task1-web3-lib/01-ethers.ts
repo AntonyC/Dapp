8775d796a4ded6869d73e8b422666e6a210a075e
Antony <antony.cn@icloud.com>
2025-09-02 23:58:46 +0800

    add filter event

--- fe-dev/task1-web3-lib/01-ethers.ts
+++ fe-dev/task1-web3-lib/01-ethers.ts
@@ -1,3 +1,4 @@
+// https://github.com/MetaNodeAcademy/Advanced4-frontend-upgrade/blob/main/Web3%E5%BC%80%E5%8F%91%E5%BA%93/ethers.js
 // 1. Run "pnpm install ethers --save" "pnpm install -D tsx"
 // 2. Install plugin "Code Runner"
 // 3. npx tsx 02-provider.ts
---
+++ fe-dev/task1-web3-lib/09-filter-event.ts
@@ -0,0 +1,74 @@
+// An event filter can contain up to 4 topic sets
+//     each of which is a condition used to filter the target events.
+
+// const filter = contract.filters.EVENT_NAME( ...args )
+
+import { ethers } from "ethers";
+import {
+  provider,
+  addrAntonyC,
+  abiAntonyC,
+  addrAccount,
+} from "./utils-sepolia.ts";
+
+const privateKey = process.env.PK || "";
+const wallet = new ethers.Wallet(privateKey, provider);
+const conAntonyC = new ethers.Contract(addrAntonyC, abiAntonyC, wallet);
+
+async function main() {
+  console.log(
+    "--Balance: ",
+    ethers.formatUnits(await conAntonyC.balanceOf(addrAccount), "ether")
+  );
+
+  let filterBinanceIn = conAntonyC.filters.Transfer(null, addrAccount);
+  console.log("--filterBinanceIn", filterBinanceIn);
+
+  conAntonyC.once(filterBinanceIn, async (res) => {
+    console.log("---------Start listen BinanceIn--------");
+    console.log(
+      `${res.args[0]} -> ${res.args[1]} ${ethers.formatUnits(
+        res.args[2],
+        "ether"
+      )}`
+    );
+    console.log(
+      "--Balance: ",
+      ethers.formatUnits(await conAntonyC.balanceOf(addrAccount), "ether")
+    );
+  });
+
+  let filterToBinanceOut = conAntonyC.filters.Transfer(addrAccount);
+  console.log("--filterToBinanceOut: ", filterToBinanceOut);
+  conAntonyC.once(filterToBinanceOut, async (res) => {
+    console.log("---------Start listen BinanceOut--------");
+    console.log(
+      `${res.args[0]} -> ${res.args[1]} ${ethers.formatUnits(
+        res.args[2],
+        "ether"
+      )}`
+    );
+    console.log(
+      "--Balance: ",
+      ethers.formatUnits(await conAntonyC.balanceOf(addrAccount), "ether")
+    );
+  });
+}
+main();
+
+// --Balance:  7.8
+// ---------Start listen BinanceOut--------
+// 0xf1C88C36Fb612Cf5D9c3f84F651bFE9049b1B927 -> 0x1329f875B2AF89dC6e1438bb8d232188F9474BA1 0.1
+// --Balance:  7.7
+// ---------Start listen BinanceIn--------
+// 0x1329f875B2AF89dC6e1438bb8d232188F9474BA1 -> 0xf1C88C36Fb612Cf5D9c3f84F651bFE9049b1B927 0.2
+// --Balance:  7.9
+
+// --filterToBinanceOut:  PreparedTopicFilter {
+//   fragment: EventFragment {
+//     type: 'event',
+//     inputs: [ [ParamType], [ParamType], [ParamType] ],
+//     name: 'Transfer',
+//     anonymous: false
+//   }
+// }
