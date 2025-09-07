a83899648dda6d063825bcd0c4eef1565ec6bc52
Antony <antony.cn@icloud.com>
2025-09-07 23:26:10 +0800

    read private data by slot

---
+++ fe-dev/task1-web3-lib/20-read-private-by-slot.ts
@@ -0,0 +1,18 @@
+import { ethers } from "ethers";
+import { provider } from "./utils-mainnet";
+
+// Arbitrum ERC20 bridge（mainnet）
+const addressBridge = "0x8315177aB297bA92A06054cE80a67Ed4DBd7ed3a"; // DAI Contract
+// slot
+const slot = `0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103`;
+
+const main = async () => {
+  console.log("Start reading slot data");
+  const privateData = await provider.getStorage(addressBridge, slot);
+  console.log(
+    "slot data: ",
+    ethers.getAddress(ethers.dataSlice(privateData, 12))
+  );
+};
+
+main();
