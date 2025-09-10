83d18b9d7098e7cd8406f185f9ceac7f3dedbbc7
Antony <antony.cn@icloud.com>
2025-09-10 22:04:59 +0800

    fix compile error

--- fe-dev/task2-stake/src/vite-env.d.ts
+++ fe-dev/task2-stake/src/vite-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+declare global {
+  var Buffer: typeof import("buffer").Buffer;
+}
+
+// There must be an export for TS to recognize this file as a module;
+// otherwise, it will be ignored
+export {};
