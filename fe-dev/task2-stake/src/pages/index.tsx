3d76a7909f247a869a53cf486bae58614bd4f522
Antony <antony.cn@icloud.com>
2025-09-14 21:57:12 +0800

    refactor

--- fe-dev/task2-stake/src/pages/index.tsx
+++ fe-dev/task2-stake/src/pages/index.tsx
@@ -1,7 +1,7 @@
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
@@ -19,13 +19,13 @@ const Home: NextPage = () => {
         <ConnectButton />
 
         <h1 className={styles.title}>
-          Welcome to <a href="https://www.rainbowkit.com">RainbowKit</a> +{' '}
-          <a href="https://wagmi.sh">wagmi</a> +{' '}
+          Welcome to <a href="https://www.rainbowkit.com">RainbowKit</a> +{" "}
+          <a href="https://wagmi.sh">wagmi</a> +{" "}
           <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
 
